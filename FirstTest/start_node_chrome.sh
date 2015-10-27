5305f5a127e54c0c7d69316d114d34f2a238a986
wojtekniedzwiecki <wniedzwiecki@gmail.com>
2015-10-27 19:42:45 +0100

    commit 9/29/2015 config of hubs/grids

---
+++ FirstTest/start_node_chrome.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar /Applications/Selenium/selenium-server-standalone-2.45.0.jar -role webdriver -nodeConfig /Users/wojtekniedzwiecki/Documents/workspace/FirstPyDev/selenium-node-chrome-mac.cfg.json
--- FirstTest/start_nodes.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-java -jar /Applications/Selenium/selenium-server-standalone-2.45.0.jar -role webdriver -nodeConfig /Users/wojtekniedzwiecki/Documents/workspace/FirstPyDev/selenium-node-chrome-mac.cfg.json &&
-java -jar /Applications/Selenium/selenium-server-standalone-2.45.0.jar -role webdriver -nodeConfig /Users/wojtekniedzwiecki/Documents/workspace/FirstPyDev/selenium-node-firefox-mac.cfg.json &&
-java -jar /Applications/Selenium/selenium-server-standalone-2.45.0.jar -role webdriver -nodeConfig /Users/wojtekniedzwiecki/Documents/workspace/FirstPyDev/selenium-node-safari-mac.cfg.json
