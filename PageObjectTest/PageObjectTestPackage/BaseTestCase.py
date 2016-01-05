6b24392fc102851a41129a2631592a86731caffb
wojtekniedzwiecki <wniedzwiecki@gmail.com>
2016-01-05 19:32:05 +0100

    commit 1/05/2016 new page object project

---
+++ PageObjectTest/PageObjectTestPackage/BaseTestCase.py
@@ -0,0 +1,16 @@
+import unittest
+from selenium import webdriver
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        # create a new Firefox session
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+
+        # navigate to the application home page
+        self.driver.get('http://empik.com/')
+
+    def tearDown(self):
+        # close the browser window
+        self.driver.quit()
---
+++ PageObjectTest/PageObjectTestPackage/base.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+class BasePage(object):
+    """ All page objects inherit from this """
+
+    def __init__(self, driver):
+        self._validate_page(driver)
+        self.driver = driver
+
+    @abstractmethod
+    def _validate_page(self, driver):
+        return
+
+    """ Regions define functionality available throughall page objects """
+    @property
+    def search(self):
+        from search import SearchRegion
+        return SearchRegion(self.driver)
+
+class InvalidPageException(Exception):
+    """ Throw this exception when you don't find the correct page """
+    pass
